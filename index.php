83f965e3a24f528e6e75ab36b0fef9552a444ce7
Petra Stefanec <petra.stefanec93@gmail.com>
2020-07-19 00:28:26 -0700

    added the css file and first draft

--- index.php
+++ index.php
@@ -1,3 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<meta charset="utf-8" />
+<link rel="stylesheet" href="style.css">
+<link href="https://fonts.googleapis.com/css2?family=Corben:wght@400;700&family=Karla:wght@400;700&display=swap" rel="stylesheet">
+<script type="module" src="nav/logo/logo_animation.js"></script>
+
+<head>
+    <title>Petra's Portfolio</title>
+</head>
+
+<body>
 <?php 
-  include_once("home.html"); 
+  include('./nav/navbar.php');
 ?>
+  <h1>body</h1>
+</body>
+
+</html>
---
+++ nav/navbar.php
@@ -0,0 +1,19 @@
+<div class="wrapper">
+  
+<div class="navbar">
+        <p>
+          <a href="" class="logo">petra.</a>
+        </p>
+        <ul>
+          <li>
+            <a href="" class="menu">ABOUT</a>
+          </li>
+          <li>
+            <a href="" class="menu">PROJECTS</a>
+          </li>
+          <li>
+            <a href="" class="menu">RESUME</a>
+          </li>
+        </ul>
+      </div>
+</div>  

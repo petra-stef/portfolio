696fefd9cbeca01679a5bef44788816260b118a2
Petra Stefanec <petra.stefanec93@gmail.com>
2021-01-26 14:42:24 +0100

    added new resume

--- nav/navbar.php
+++ nav/navbar.php
@@ -1,7 +1,7 @@
 <div class="navbar">
   <div class="wow fadeInDown">
     <a href="./index.php" class="logo">petra.</a>
-    <a href="https://drive.google.com/file/d/1m35qrlB6Ysyu6bJe5Ff3XA4HOzcDdA32/view?usp=sharing" id="project" class="menu" target="_blank">RESUME</a>
+    <a href="https://drive.google.com/file/d/1W5wjJsQzSlNk2JCf7RJh58IZQBFG2m7T/view?usp=sharing" id="project" class="menu" target="_blank">RESUME</a>
     <a href="./about.php" class="menu" id="about">ABOUT</a>
     <a href="./index.php" class="menu active" id="portfolio">PORTFOLIO</a>
   </div>
